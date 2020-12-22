d6d6ec4160dfdeb517694707f4a41ae60df966fe
Rex Gao <rexgao2013@gmail.com>
2020-12-22 14:13:43 -0800

    create user and tweet routes

---
+++ app.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const app = express();
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World, JavaScript!!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
