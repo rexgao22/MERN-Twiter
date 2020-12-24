67e3bc1afc482f4de747f2917abe01e1deb278f8
Rex Gao <rexgao2013@gmail.com>
2020-12-24 14:29:00 -0800

    create Auth and Protected routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/app";
 import Root from "./components/root";
 import configureStore from "./store/store";
 import jwt_decode from "jwt-decode";
