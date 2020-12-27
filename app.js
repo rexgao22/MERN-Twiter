48f28ce75201cb1515fa9d730e6cf99f8f729a14
Rex Gao <rexgao2013@gmail.com>
2020-12-26 23:32:43 -0800

    change direction

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
 const app = express();
 const db = require("./config/keys").mongoURI;
-const User = require("./models/User");
 const passport = require("passport");
 const path = require('path');
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -20,16 +20,20 @@ require("./config/passport")(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('frontend/build'));
-  app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(_dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+
+
+
